c45860ed38387d557d61c058be74fb083234c4e6
Macbaren <macbaren123@gmail.com>
2022-02-11 21:35:25 +0300

    feat fin brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const checkStr = [str[0]];
+  const strToArr = str.split('');
+  const configObj = Object.fromEntries(bracketsConfig);
+
+  const isClosing = (br) => Object.values(configObj).includes(br);
+
+  for (let i = 1; i < strToArr.length; i += 1) {
+    checkStr.push(strToArr[i]);
+
+    while (
+      isClosing(checkStr[checkStr.length - 1]) &&
+      configObj[checkStr[checkStr.length - 2]] === checkStr[checkStr.length - 1]
+    ) {
+      checkStr.splice(-2);
+    }
+  }
+  return checkStr.length === 0;
+};
